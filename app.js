08b1259e4e2186f65bef68a0c749e572e3c6c5c5
TheTulatula <tnidam@gmail.com>
2024-12-24 20:19:59 +0000

    ai added

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: [
-      "Paws that prance where shadows go.",
-      "Silent hunters, sleek and sly,",
-      "Secrets hidden in their eye.",
-      "A purring heart, a quiet show.",
-    ],
+    strings: response.data.answer,
     autoStart: true,
     delay: 40,
     loop: false,
     deleteSpeed: 0,
     pauseFor: 500,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let userPrompt = document.querySelector("#userPrompt");
+
+  let apiKey = "f80eot135d2ba84faf905b0d90035259";
+  let context =
+    "yoa are an ai expert poet who can write original short poems on every topic requested. the poem can be up to 4 lines length. Please generate the poem in basic HTML format only, without any code block notation or backticks, and separate each line with a <br/> .please do not write anything else beside the poem, no previews needed. sign the poem with `SheCodes AI` in <strong> element.";
+  let prompt = `generate a poem about ${userPrompt.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`context:${context}`);
+  console.log(`prompt:${prompt}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 let peomFormElement = document.querySelector("#poemGenerator");
 peomFormElement.addEventListener("submit", generatePoem);
