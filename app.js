a2528bb71b854befc3736e43aa0a6192d571b1c3
TheTulatula <tnidam@gmail.com>
2024-12-25 17:49:23 +0000

    improved user experience

--- app.js
+++ app.js
@@ -15,9 +15,13 @@ function generatePoem(event) {
 
   let apiKey = "f80eot135d2ba84faf905b0d90035259";
   let context =
-    "yoa are an ai expert poet who can write original short poems on every topic requested. the poem can be up to 4 lines length. Please generate the poem in basic HTML format only, without any code block notation or backticks, and separate each line with a <br/> .please do not write anything else beside the poem, no previews needed. sign the poem with `SheCodes AI` in <strong> element.";
+    "yoa are an ai expert poet who can write original short poems on every topic requested. the poem can be up to 4 lines length. Please generate the poem in basic HTML format only, without any code block notation or backticks, and separate each line with a <br/> .please do not write anything else beside the poem, no previews needed. sign the poemin a new line with `SheCodes AI` in <strong> element.";
   let prompt = `generate a poem about ${userPrompt.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">Generating a poem about "${userPrompt.value}"...</div>`;
 
   console.log("Generating poem");
   console.log(`context:${context}`);
